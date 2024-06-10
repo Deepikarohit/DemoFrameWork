bdeb0e9e662cbc9f1ba8b5eb7c28775194d6031c
deepi <deepi@MIVANSH>
2024-06-10 15:39:54 +0530

    First Push

---
+++ DemoFramework/src/test/java/contacttest/Tocreate_Contact.java
@@ -0,0 +1,12 @@
+package contacttest;
+
+public class Tocreate_Contact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Contact page got created");
+
+	}
+
+}
